dc86dc549ca1fce58ddac8c6a6d8c83fe6863761
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-11-08 19:24:26 +0300

    added pitches form

--- app/main/forms.py
+++ app/main/forms.py
@@ -16,7 +16,7 @@ class UpdateProfileForm(FlaskForm):
 
 class PostForm(FlaskForm):
     post_title = StringField('Pitch title', validators=[Required()])
-    pitch_category = SelectField('Pitch category',choices=[('Select a category','Select a category'),('Pickup lines', 'Pickup lines'),('Interview','Interview'),('Product','Product'),('Promotions','Promotions')], validators=[Required()])
+    category = SelectField('Pitch category',choices=[('Select a category','Select a category'),('Pickup lines', 'Pickup lines'),('Interview','Interview'),('Product','Product'),('Promotions','Promotions')], validators=[Required()])
     comment = StringField('What is in your mind?')
     submit = SubmitField('Submit')
     
